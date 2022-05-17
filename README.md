1117ed233f627b7518096cadfca97d24cc23add9
Dev, Vasu <vasu_dev2@optum.com>
2022-05-18 04:32:26 +0530

    added files

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 An API for CRUD Operations on any Product
 
 ## Written in Golang with Gorrila Mux and SQL Database
+
+## Pre Requesites
+* You should be connected to a live SQL Server (local server or remote server)
+* settings for SQL connection can be done in `./datasources/items_db`
+* Table `items` should already be existing in the database (setup in step 2)
+
+## Setting up
+* Run `go get .` in the application root folder
+* Run `go run main.go`
--- main.go
+++ main.go
@@ -17,23 +17,26 @@ func main() {
 	//ping system
 	router.HandleFunc("/ping", controllers.Ping).Methods(http.MethodGet)
 
+
+	// get items and create items
 	router.HandleFunc("/items", controllers.ItemsController.Create).Methods(http.MethodPost)
+	router.HandleFunc("/items", controllers.ItemsController.List).Methods(http.MethodGet)
 	router.HandleFunc("/items", controllers.ItemsController.Create).Methods(http.MethodOptions)
 
+	//get items by userId
 	router.HandleFunc("/items/mine", controllers.ItemsController.GetByUserId).Methods(http.MethodGet)
 	router.HandleFunc("/items/mine", controllers.ItemsController.GetByUserId).Methods(http.MethodOptions)
 
+	//CRUD for items
 	router.HandleFunc("/items/{itemId}", controllers.ItemsController.Delete).Methods(http.MethodDelete)
 	router.HandleFunc("/items/{itemId}", controllers.ItemsController.Get).Methods(http.MethodGet)
 	router.HandleFunc("/items/{itemId}", controllers.ItemsController.Update).Methods(http.MethodPut)
-	router.HandleFunc("/items", controllers.ItemsController.List).Methods(http.MethodGet)
 
 
 
 	srv := &http.Server{
 		Handler: router,
 		Addr: "127.0.0.1:8000",
-		// Addr: "0.0.0.0:8000",
 	}
 
 	if err := srv.ListenAndServe(); err != nil {
